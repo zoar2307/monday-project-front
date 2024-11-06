f73483a6268d46f0bb534df4cc234866dc68ee6c
Avi Museri <avimus@my.hit.ac.il>
2024-11-06 17:49:23 +0200

    Project starter

---
+++ src/cmps/AppFooter.jsx
@@ -0,0 +1,6 @@
+export function AppFooter() {
+    return (
+        <footer className="footer">
+            <p>&copy; 2024 Joy Web Store. All rights reserved.</p>
+        </footer>)
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+
+export function HomePage() {
+    return (
+        <div className="homepage">
+
+            <p>Home Page Details....</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, minus sint adipisci iusto cupiditate eum quasi perferendis, sunt atque laborum, saepe fugit consequuntur odit incidunt veniam excepturi voluptas illum. Saepe.</p>
+        </div>
+    )
+}
