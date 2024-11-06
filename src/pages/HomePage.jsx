820854194640801fc0605b27c84a5bc03814799a
dgalilove <dgalilove@gmail.com>
2024-11-06 20:29:52 +0200

    home edit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,19 @@
 export function HomePage() {
   return (
     <div className="homepage">
+    <nav className="home-nav flex justify-between">
+      <p>da</p>
+      <p>Log in</p>
+    </nav>
+    <div className="home-content flex flex-column align-center">
+
       <h1>Your go-to work platform</h1>
       <p>
         Streamline workflows and gain clear visibility across teams <br></br>to
         make strategic decisions with confidence
       </p>
+      <button>Get Started <i class="fa-solid fa-arrow-right"></i> </button>
+    </div>
     </div>
   )
 }
