55ccc4771f7f11e20625f57daae95db4e0ae07f3
zoar2307 <zoar2307@gmail.com>
2024-11-06 20:26:20 +0200

    Add style

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import welcomeSignUp from '../assets/img/welcome-to-monday.avif'
 
 export function Signup() {
@@ -15,33 +16,32 @@ export function Signup() {
 
                     <div className="form-container">
                         <div className="button-container">
-                            <button>
+                            <button className='google-button btn'>
                                 <img className="google-svg" alt="Continue with Google" src="https://dapulse-res.cloudinary.com/image/upload/remote_logos/995426/google-icon.svg" />
                                 <span>Continue with Google</span>
                             </button>
                         </div>
 
-
                         <div className="or-container">
                             <div className="or-line"></div>
-                            Or
+                            <div>Or</div>
                             <div className="or-line"></div>
                         </div>
 
                         <form >
                             <div className="input-container">
-                                <input type="text" placeholder="name@compamy.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
+                                <input className='btn' type="text" placeholder="name@compamy.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
                             </div>
 
-                            <div className="sign-up-button">
-                                <button>Continue</button>
+                            <div className="continue-container">
+                                <button className='continue-button btn'>Continue</button>
                             </div>
 
                         </form>
                     </div>
                 </div>
                 <div className="signup-footer">
-                    <span>Already have an account?</span> <span>Log in</span>
+                    <span>Already have an account?</span> <span><Link>Log in</Link></span>
                 </div>
             </div>
 
