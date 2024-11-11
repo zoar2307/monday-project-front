1c96d8432a6982667eceebd2c6399d991141c0b6
dgalilove <dgalilove@gmail.com>
2024-11-11 16:44:07 +0200

    Add Rename is SideBar

--- src/cmps/SideBar.jsx
+++ src/cmps/SideBar.jsx
@@ -26,6 +26,7 @@ export function SideBar({ onSidebarToggle }) {
     const [modalBoardId, setModalBoardId] = useState(null)
     const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 })
     const [isStarred, setIsStarred] = useState(false)
+    const [renameMode, setRenameMode] = useState(null) 
     const [boardTitles, setBoardTitles] = useState(
         boards.reduce((acc, board) => {
             acc[board._id] = board.title
@@ -178,18 +179,8 @@ export function SideBar({ onSidebarToggle }) {
         }
     }
 
-    async function onRename(boardId) {
-        const newTitle = prompt("Enter the new board name:")
-        if (newTitle) {
-            try {
-                await updateBoard(boardId, { title: newTitle })
-                await loadBoards()
-                closeModal()
-            } catch (err) {
-                console.error("Error renaming board:", err)
-            }
-        }
-    }
+
+
 
     async function onAddToFavorites(boardId) {
         console.log("Adding to favorites:", boardId)
@@ -209,16 +200,16 @@ export function SideBar({ onSidebarToggle }) {
 
 
 
-    const openModal = (board, event) => {
-        setModalBoardId(board._id);
-        setIsStarred(board.isStarred);
-        const { top, left, height } = event.currentTarget.getBoundingClientRect();
+    function openModal(board, event) {
+        setModalBoardId(board._id)
+        setIsStarred(board.isStarred)
+        const { top, left, height } = event.currentTarget.getBoundingClientRect()
         setModalPosition({
             top: top + height + 10 + window.scrollY,
             left: left + window.scrollX,
-        });
-        setIsModalOpen(true);
-    };
+        })
+        setIsModalOpen(true)
+    }
 
 
 
@@ -244,6 +235,7 @@ export function SideBar({ onSidebarToggle }) {
             window.removeEventListener("mousedown", handleClickOutside)
         }
     }, [isModalOpen])
+    
 
     const handleTitleChange = (boardId, newTitle) => {
         setBoardTitles((prevTitles) => ({
@@ -255,21 +247,18 @@ export function SideBar({ onSidebarToggle }) {
     const handleTitleSave = async (boardId) => {
         try {
             const updatedTitle = boardTitles[boardId]
-            
             if (updatedTitle) {
-                // Ensure we're passing an object with both `boardId` and `title`
-                const updatedBoardData = { _id: boardId, title: updatedTitle }
-    
-                await saveBoard(updatedBoardData)
-                await loadBoards()
-                closeModal()  // Ensure modal closes after save
+                const updatedBoardData = { ...boards.find(b => b._id === boardId), title: updatedTitle }
+                await saveBoard(updatedBoardData) // Save the board with the new title
+                setRenameMode(null) // Exit rename mode
             }
         } catch (err) {
             console.error("Error updating title:", err)
         }
     }
     
-    
+
+
 
     const hiddenClass = favoritesIsOpen ? 'hidden' : ''
 
@@ -368,54 +357,71 @@ export function SideBar({ onSidebarToggle }) {
                 </button>
             </div>
 
-            {boards && <div className={`boards ${hiddenClass}`}>
-                {filterByToEdit.title ?
-                    filteredBoards.map(board => {
-                        return <div onClick={() => onBoardClick(board._id)} key={board._id} className={`board-wrapper ${pathname === `/board/${board._id}` ? 'board active' : 'board'}`} >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="19" height="19" aria-hidden="true" data-testid="icon"><path d="M7.5 4.5H16C16.2761 4.5 16.5 4.72386 16.5 5V15C16.5 15.2761 16.2761 15.5 16 15.5H7.5L7.5 4.5ZM6 4.5H4C3.72386 4.5 3.5 4.72386 3.5 5V15C3.5 15.2761 3.72386 15.5 4 15.5H6L6 4.5ZM2 5C2 3.89543 2.89543 3 4 3H16C17.1046 3 18 3.89543 18 5V15C18 16.1046 17.1046 17 16 17H4C2.89543 17 2 16.1046 2 15V5Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd" /></svg>
-                            <input
-                                type="text"
-                                value={boardTitles[board._id]}
-                                onChange={(e) => handleTitleChange(board._id, e.target.value)}
-                                onBlur={() => handleTitleSave(board._id)}  // Save title on blur
-                                placeholder="Rename board"
-                            />
-                            <div className="dots" onClick={(e) => { e.stopPropagation(); openModal(board, e) }}>
-                                <button>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="20" height="20" role="img" aria-hidden="true"><path d="M6 10.5C6 11.3284 5.32843 12 4.5 12 3.67157 12 3 11.3284 3 10.5 3 9.67157 3.67157 9 4.5 9 5.32843 9 6 9.67157 6 10.5zM11.8333 10.5C11.8333 11.3284 11.1618 12 10.3333 12 9.50492 12 8.83334 11.3284 8.83334 10.5 8.83334 9.67157 9.50492 9 10.3333 9 11.1618 9 11.8333 9.67157 11.8333 10.5zM17.6667 10.5C17.6667 11.3284 16.9951 12 16.1667 12 15.3383 12 14.6667 11.3284 14.6667 10.5 14.6667 9.67157 15.3383 9 16.1667 9 16.9951 9 17.6667 9.67157 17.6667 10.5z" fill="currentColor" /></svg>
-                                </button>
-                            </div>
-                        </div>
-                    })
-                    :
-                    boards.map(board => {
-                        return <div onClick={() => onBoardClick(board._id)} key={board._id} className={`board-wrapper ${pathname === `/board/${board._id}` ? 'board active' : 'board'}`}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="19" height="19" aria-hidden="true" data-testid="icon"><path d="M7.5 4.5H16C16.2761 4.5 16.5 4.72386 16.5 5V15C16.5 15.2761 16.2761 15.5 16 15.5H7.5L7.5 4.5ZM6 4.5H4C3.72386 4.5 3.5 4.72386 3.5 5V15C3.5 15.2761 3.72386 15.5 4 15.5H6L6 4.5ZM2 5C2 3.89543 2.89543 3 4 3H16C17.1046 3 18 3.89543 18 5V15C18 16.1046 17.1046 17 16 17H4C2.89543 17 2 16.1046 2 15V5Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd" /></svg>
-                            <input
-                                type="text"
-                                value={boardTitles[board._id]}
-                                onChange={(e) => handleTitleChange(board._id, e.target.value)}
-                                onBlur={() => handleTitleSave(board._id)}  // Save title on blur
-                                placeholder="Rename board"
-                            />
-                            <div className="dots" onClick={(e) => { e.stopPropagation(); openModal(board, e) }}>
-                                <button>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="20" height="20" role="img" aria-hidden="true"><path d="M6 10.5C6 11.3284 5.32843 12 4.5 12 3.67157 12 3 11.3284 3 10.5 3 9.67157 3.67157 9 4.5 9 5.32843 9 6 9.67157 6 10.5zM11.8333 10.5C11.8333 11.3284 11.1618 12 10.3333 12 9.50492 12 8.83334 11.3284 8.83334 10.5 8.83334 9.67157 9.50492 9 10.3333 9 11.1618 9 11.8333 9.67157 11.8333 10.5zM17.6667 10.5C17.6667 11.3284 16.9951 12 16.1667 12 15.3383 12 14.6667 11.3284 14.6667 10.5 14.6667 9.67157 15.3383 9 16.1667 9 16.9951 9 17.6667 9.67157 17.6667 10.5z" fill="currentColor" /></svg>
-                                </button>
-                            </div>
-                        </div>
-                    })
-                }
+            {boards && (
+                <div className={`boards ${hiddenClass}`}>
+                    {filterByToEdit.title
+                        ? filteredBoards.map((board) => (
+                            <div
+                                onClick={() => onBoardClick(board._id)}
+                                key={board._id}
+                                className={`board-wrapper ${pathname === `/board/${board._id}` ? 'board active' : 'board'}`}
+                            >
 
-            </div>
-            }
+                                {renameMode === board._id ? (
+                                    <input
+                                        type="text"
+                                        value={boardTitles[board._id]}
+                                        onChange={(e) => handleTitleChange(board._id, e.target.value)}
+                                        onBlur={() => handleTitleSave(board._id)}
+                                        autoFocus
+                                    />
+                                ) : (
+                                    <p>{board.title}</p>
+                                )}
+                                <div className="dots" onClick={(e) => { e.stopPropagation(); openModal(board, e) }}>
+                                    <button>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="20" height="20" role="img" aria-hidden="true"><path d="M6 10.5C6 11.3284 5.32843 12 4.5 12 3.67157 12 3 11.3284 3 10.5 3 9.67157 3.67157 9 4.5 9 5.32843 9 6 9.67157 6 10.5zM11.8333 10.5C11.8333 11.3284 11.1618 12 10.3333 12 9.50492 12 8.83334 11.3284 8.83334 10.5 8.83334 9.67157 9.50492 9 10.3333 9 11.1618 9 11.8333 9.67157 11.8333 10.5zM17.6667 10.5C17.6667 11.3284 16.9951 12 16.1667 12 15.3383 12 14.6667 11.3284 14.6667 10.5 14.6667 9.67157 15.3383 9 16.1667 9 16.9951 9 17.6667 9.67157 17.6667 10.5z" fill="currentColor" /></svg>
+                                    </button>
+                                </div>
+                            </div>
+                        ))
+                        : boards.map((board) => (
+                            <div
+                                onClick={() => onBoardClick(board._id)}
+                                key={board._id}
+                                className={`board-wrapper ${pathname === `/board/${board._id}` ? 'board active' : 'board'}`}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="19" height="19" aria-hidden="true" className="icon_1360dfb99d" data-testid="icon"><path d="M7.5 4.5H16C16.2761 4.5 16.5 4.72386 16.5 5V15C16.5 15.2761 16.2761 15.5 16 15.5H7.5L7.5 4.5ZM6 4.5H4C3.72386 4.5 3.5 4.72386 3.5 5V15C3.5 15.2761 3.72386 15.5 4 15.5H6L6 4.5ZM2 5C2 3.89543 2.89543 3 4 3H16C17.1046 3 18 3.89543 18 5V15C18 16.1046 17.1046 17 16 17H4C2.89543 17 2 16.1046 2 15V5Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd" /></svg>
+                                {renameMode === board._id ? (
+                                    <input
+                                        type="text"
+                                        value={boardTitles[board._id]}
+                                        onChange={(e) => handleTitleChange(board._id, e.target.value)}
+                                        onBlur={() => handleTitleSave(board._id)}  // Save title on blur
+                                        autoFocus
+                                    />
+                                ) : (
+                                    <p>{board.title}</p>
+                                )}
+                                <div className="dots" onClick={(e) => { e.stopPropagation(); openModal(board, e) }}>
+                                    <button>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="20" height="20" role="img" aria-hidden="true"><path d="M6 10.5C6 11.3284 5.32843 12 4.5 12 3.67157 12 3 11.3284 3 10.5 3 9.67157 3.67157 9 4.5 9 5.32843 9 6 9.67157 6 10.5zM11.8333 10.5C11.8333 11.3284 11.1618 12 10.3333 12 9.50492 12 8.83334 11.3284 8.83334 10.5 8.83334 9.67157 9.50492 9 10.3333 9 11.1618 9 11.8333 9.67157 11.8333 10.5zM17.6667 10.5C17.6667 11.3284 16.9951 12 16.1667 12 15.3383 12 14.6667 11.3284 14.6667 10.5 14.6667 9.67157 15.3383 9 16.1667 9 16.9951 9 17.6667 9.67157 17.6667 10.5z" fill="currentColor" /></svg>
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                </div>
+            )}
             {isModalOpen && (
                 <BoardOptionsModal
-                    onClose={closeModal}
+                    onClose={() => setIsModalOpen(false)}
                     boardId={modalBoardId}
                     onDelete={onDelete}
                     modalPosition={modalPosition}
                     onAddToFavorites={onAddToFavorites}
+                    onRename={() => setRenameMode(modalBoardId)}
+                    handleTitleChange={handleTitleChange}
+                    handleTitleSave={handleTitleSave}
                     isStarred={isStarred}
                 />
             )}
