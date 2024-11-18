2ba36bcaf090bf92738b194f956167f560d180f3
zoar2307 <zoar2307@gmail.com>
2024-11-19 00:52:54 +0200

    Add kanban socket

--- src/cmps/Kanban.jsx
+++ src/cmps/Kanban.jsx
@@ -4,13 +4,31 @@ import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { loadBoard, resetFilterBy } from "../store/actions/board.actions";
 import { KanbanList } from "./KanbanList";
+import { SOCKET_EVENT_BOARD_UPDATE } from "../services/socket.service";
+import { SET_BOARD } from "../store/reducers/board.reducer";
+import { useDispatch } from "react-redux";
 
 export function Kanban() {
     const board = useSelector(storeState => storeState.boardModule.currBoard)
     const filterBy = useSelector(storeState => storeState.boardModule.filterBy)
+    const dispatch = useDispatch()
 
     const { boardId, taskId } = useParams()
     const navigate = useNavigate()
+
+
+    useEffect(() => {
+
+
+        socketService.on(SOCKET_EVENT_BOARD_UPDATE, updatedBoard => {
+            // loadBoards()
+            dispatch({ type: SET_BOARD, board: updatedBoard })
+        })
+
+        return () => {
+            socketService.off(SOCKET_EVENT_BOARD_UPDATE)
+        }
+    }, [])
 
     useEffect(() => {
         if (boardId) initBoard()
