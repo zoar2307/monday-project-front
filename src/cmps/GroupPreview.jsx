de1df5440fb78a2e6450bc45352014dc4e896706
dgalilove <dgalilove@gmail.com>
2024-11-18 15:14:11 +0200

    Add close group

--- src/cmps/GroupPreview.jsx
+++ src/cmps/GroupPreview.jsx
@@ -35,6 +35,8 @@ export function GroupPreview({
     setModalOpen(!isModalOpen)
   }
   const [isHovered, setIsHovered] = useState(false)
+  const [isGroupCollapsed, setIsGroupCollapsed] = useState(false)
+
 
   const cmpsOrder = board.cmpsLabels.map(label => label.type)
 
@@ -66,6 +68,10 @@ export function GroupPreview({
         break
     }
     setNewTask(prevFilter => ({ ...prevFilter, title: value }))
+  }
+
+  const toggleGroupCollapse = () => {
+    setIsGroupCollapsed(prevState => !prevState)
   }
 
   const handleTitleChange = (e) => setGroupTitle(e.target.value)
@@ -184,138 +190,208 @@ export function GroupPreview({
                     )}
                     <img src={dots} alt="dots" />
                   </button>
-                  <p style={{ color: group.color }}>
-                    <i className="fa-solid fa-chevron-down"></i>
-                  </p>
 
-                  {isEditGroupTitle ? (
-                    <form onSubmit={saveTitle}>
-                      <input
-                        type="text"
-                        value={groupTitle}
-                        onChange={handleTitleChange}
-                        onBlur={saveTitle}
-                        style={{ color: group.color, width: '500px', maxWidth: '500px' }}
-                        className="group-title"
-                        autoFocus
-                      />
-                    </form>
-                  ) : (
-                    <h4
-                      className="group-title"
-                      onClick={() => setIsEditGroupTitle(true)}
-                      style={{ color: group.color, borderColor: group.color }}
-                    >
-                      {groupTitle}
-                    </h4>
+                  {!isGroupCollapsed && (
+                    <>
+
+                      <p
+                        style={{ color: group.color }}
+                        onClick={toggleGroupCollapse}
+                        className="toggle-collapse"
+                      >
+                        <i
+                          className={`fa-solid ${isGroupCollapsed ? 'fa-chevron-right' : 'fa-chevron-down'}`}
+                        ></i>
+                      </p>
+
+                      {isEditGroupTitle ? (
+                        <form onSubmit={saveTitle}>
+                          <input
+                            type="text"
+                            value={groupTitle}
+                            onChange={handleTitleChange}
+                            onBlur={saveTitle}
+                            style={{ color: group.color, width: '500px', maxWidth: '500px' }}
+                            className="group-title"
+                            autoFocus
+                          />
+                        </form>
+                      ) : (
+                        <h4
+                          className="group-title"
+                          onClick={() => setIsEditGroupTitle(true)}
+                          style={{ color: group.color, borderColor: group.color }}
+                        >
+                          {groupTitle}
+                        </h4>
+                      )}
+                    </>
+
                   )}
+
 
                   <span className="tasks-count">{tasks.length} Task{tasks.length !== 1 && 's'}</span>
                 </div>
 
-
-                <div className='group-header ' >
-                  <div className='check-label'
-                    style={{ '--before-color': group.color }}
-                  >
-                    <input type="checkbox" /></div >
-                  <div className='task-title'>Task
-
-                    {board.cmpsLabels.length === 0 &&
-                      <div className='add-label-container'>
-                        <button ref={modalAddBtnRef} onClick={() => onModal('open-add')} className='add-label'>
-                          <svg className={`${labelModal.type === 'add' && labelModal.isDisplay && 'opened-modal'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="20" height="20" aria-hidden="true" data-testid="icon"><g id="Icon / Basic / Add"><path id="Union" d="M10 2.25C10.4142 2.25 10.75 2.58579 10.75 3V9.25H17C17.4142 9.25 17.75 9.58579 17.75 10C17.75 10.4142 17.4142 10.75 17 10.75H10.75V17C10.75 17.4142 10.4142 17.75 10 17.75C9.58579 17.75 9.25 17.4142 9.25 17V10.75H3C2.58579 10.75 2.25 10.4142 2.25 10C2.25 9.58579 2.58579 9.25 3 9.25H9.25V3C9.25 2.58579 9.58579 2.25 10 2.25Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd" /></g></svg>
-                        </button>
-                        {labelModal.type === 'add' && labelModal.isDisplay && <LabelModal board={board} type={'add'} setLabelModal={setLabelModal} labelModal={labelModal} modalAddBtnRef={modalAddBtnRef} />}
-                      </div>
-                    }
-                  </div>
-                  <Droppable
-                    droppableId={groupId + 'label'}
-                    direction='horizontal'
-                    type="labels"
-                  >
-                    {(provided, snapshot) => (
-                      <div
-                        className={`labels`}
-                        ref={provided.innerRef}
-                        {...provided.droppableProps}
-                        style={{
-                          '--labels': board.cmpsLabels.length
-                        }}
-                      >
-
-                        {board.cmpsLabels.map((label, idx) => {
-                          return (
-                            <Draggable draggableId={label.id + groupId} index={idx} key={label.id + groupId}>
-                              {(provided, snapshot) => (
-                                <div key={label.id + groupId} className={`label ${isDragOn && 'drag'}`}
-                                  {...provided.draggableProps}
-                                  ref={provided.innerRef}
-                                  {...provided.dragHandleProps}
-                                >
-                                  {setIsDragOn(snapshot.isDragging)}
-                                  {label.title}
-                                  <div className='dots-container'>
-                                    <button ref={modalRemoveBtnRef} onClick={() => onModal('open-remove', label.id, groupId)}
-                                      className={`dots 
-                                         ${labelModal.isDisplay
-                                        && labelModal.gId === groupId
-                                        && labelModal.lId === label.id
-                                        && 'opened-modal'}`}>
-                                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="20" height="20" role="img" aria-hidden="true"><path d="M6 10.5C6 11.3284 5.32843 12 4.5 12 3.67157 12 3 11.3284 3 10.5 3 9.67157 3.67157 9 4.5 9 5.32843 9 6 9.67157 6 10.5zM11.8333 10.5C11.8333 11.3284 11.1618 12 10.3333 12 9.50492 12 8.83334 11.3284 8.83334 10.5 8.83334 9.67157 9.50492 9 10.3333 9 11.1618 9 11.8333 9.67157 11.8333 10.5zM17.6667 10.5C17.6667 11.3284 16.9951 12 16.1667 12 15.3383 12 14.6667 11.3284 14.6667 10.5 14.6667 9.67157 15.3383 9 16.1667 9 16.9951 9 17.6667 9.67157 17.6667 10.5z" fill="currentColor" /></svg>
-                                    </button>
-                                    {labelModal.type === 'remove'
-                                      && labelModal.isDisplay
-                                      && labelModal.gId === groupId
-                                      && labelModal.lId === label.id
-                                      && < LabelModal modalRemoveBtnRef={modalRemoveBtnRef} board={board} type={'remove'} labelId={label.id} setLabelModal={setLabelModal} labelModal={labelModal} />}
-                                  </div>
-                                  {idx === board.cmpsLabels.length - 1 &&
-                                    <div className='add-label-container'>
-                                      <button ref={modalAddBtnRef} onClick={() => onModal('open-add')} className='add-label'>
-                                        <svg className={`${labelModal.type === 'add' && labelModal.isDisplay && 'opened-modal'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="20" height="20" aria-hidden="true" data-testid="icon"><g id="Icon / Basic / Add"><path id="Union" d="M10 2.25C10.4142 2.25 10.75 2.58579 10.75 3V9.25H17C17.4142 9.25 17.75 9.58579 17.75 10C17.75 10.4142 17.4142 10.75 17 10.75H10.75V17C10.75 17.4142 10.4142 17.75 10 17.75C9.58579 17.75 9.25 17.4142 9.25 17V10.75H3C2.58579 10.75 2.25 10.4142 2.25 10C2.25 9.58579 2.58579 9.25 3 9.25H9.25V3C9.25 2.58579 9.58579 2.25 10 2.25Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd" /></g></svg>
-                                      </button>
-                                      {labelModal.type === 'add' && labelModal.isDisplay && <LabelModal board={board} type={'add'} setLabelModal={setLabelModal} labelModal={labelModal} modalAddBtnRef={modalAddBtnRef} />}
-                                    </div>
-                                  }
-                                </div>
-
-                              )}
-                            </Draggable>
-                          )
-
-                        })}
-                        {provided.placeholder}
-
-                      </div>
-                    )}
-                  </Droppable>
-
-
-                </div  >
-
-
               </header>
 
+              <div
+                className={`group-header ${isGroupCollapsed ? 'collapsed' : ''}`}
+              >
 
-              <TaskList group={group} />
+                {!isGroupCollapsed && (
+                  <>
+
+                    <div className='check-label'
+                      style={{ '--before-color': group.color }}
+                    >
+                      <input type="checkbox" /></div >
+                    <div className='task-title'>Task</div>
+                  </>
+                )}
+
+                {isGroupCollapsed && (
+                  <div className="col-group-title" style={{ '--before-color': group.color }}>
+
+                    <p
+                      style={{ color: group.color }}
+                      onClick={toggleGroupCollapse}
+                      className="toggle-collapse"
+                    >
+                      <i
+                        className={`fa-solid ${isGroupCollapsed ? 'fa-chevron-right' : 'fa-chevron-down'}`}
+                      ></i>
+                    </p>
+
+                    {isEditGroupTitle ? (
+                      <form onSubmit={saveTitle}>
+                        <input
+                          type="text"
+                          value={groupTitle}
+                          onChange={handleTitleChange}
+                          onBlur={saveTitle}
+                          style={{ color: group.color, width: '500px', maxWidth: '500px' }}
+                          className="group-title"
+                          autoFocus
+                        />
+                      </form>
+                    ) : (
+
+                      <h4
+                        className="group-title"
+                        onClick={() => setIsEditGroupTitle(true)}
+                        style={{ color: group.color, borderColor: group.color }}
+                      >
+                        {groupTitle}
+                      </h4>
+
+                    )}
+
+
+
+                    <span className="tasks-count">{tasks.length} Task{tasks.length !== 1 && 's'}</span>
+                  </div>
+
+
+
+                )}
+
+                
+
+                <Droppable
+                  droppableId={groupId + 'label'}
+                  direction='horizontal'
+                  type="labels"
+                >
+                  {(provided, snapshot) => (
+                    <div
+                      className={`labels`}
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      style={{
+                        '--labels': board.cmpsLabels.length
+                      }}
+                    >
+
+                      {board.cmpsLabels.map((label, idx) => {
+                        return (
+                          <Draggable draggableId={label.id + groupId} index={idx} key={label.id + groupId}>
+                            {(provided, snapshot) => (
+                              <div key={label.id + groupId} className={`label ${isGroupCollapsed ? 'collapsed' : ''} ${isDragOn && 'drag'}`}
+                                {...provided.draggableProps}
+                                ref={provided.innerRef}
+                                {...provided.dragHandleProps}
+                              >
+                                {setIsDragOn(snapshot.isDragging)}
+                                {label.title}
+                                <div className='dots-container'>
+                                  <button ref={modalRemoveBtnRef} onClick={() => onModal('open-remove', label.id, groupId)}
+                                    className={`dots 
+                                         ${labelModal.isDisplay
+                                      && labelModal.gId === groupId
+                                      && labelModal.lId === label.id
+                                      && 'opened-modal'}`}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="20" height="20" role="img" aria-hidden="true"><path d="M6 10.5C6 11.3284 5.32843 12 4.5 12 3.67157 12 3 11.3284 3 10.5 3 9.67157 3.67157 9 4.5 9 5.32843 9 6 9.67157 6 10.5zM11.8333 10.5C11.8333 11.3284 11.1618 12 10.3333 12 9.50492 12 8.83334 11.3284 8.83334 10.5 8.83334 9.67157 9.50492 9 10.3333 9 11.1618 9 11.8333 9.67157 11.8333 10.5zM17.6667 10.5C17.6667 11.3284 16.9951 12 16.1667 12 15.3383 12 14.6667 11.3284 14.6667 10.5 14.6667 9.67157 15.3383 9 16.1667 9 16.9951 9 17.6667 9.67157 17.6667 10.5z" fill="currentColor" /></svg>
+                                  </button>
+                                  {labelModal.type === 'remove'
+                                    && labelModal.isDisplay
+                                    && labelModal.gId === groupId
+                                    && labelModal.lId === label.id
+                                    && < LabelModal modalRemoveBtnRef={modalRemoveBtnRef} board={board} type={'remove'} labelId={label.id} setLabelModal={setLabelModal} labelModal={labelModal} />}
+                                </div>
+                              </div>
+                               
+
+                            )}
+                          </Draggable>
+                        )
+
+                      })}
+                      {provided.placeholder}
+
+                    </div>
+
+                  )}
+                </Droppable>
+                                 <div className='add-label-container'>
+                                    <button ref={modalAddBtnRef} onClick={() => onModal('open-add')} className='add-label'>
+                                      <svg className={`${labelModal.type === 'add' && labelModal.isDisplay && 'opened-modal'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="20" height="20" aria-hidden="true" data-testid="icon"><g id="Icon / Basic / Add"><path id="Union" d="M10 2.25C10.4142 2.25 10.75 2.58579 10.75 3V9.25H17C17.4142 9.25 17.75 9.58579 17.75 10C17.75 10.4142 17.4142 10.75 17 10.75H10.75V17C10.75 17.4142 10.4142 17.75 10 17.75C9.58579 17.75 9.25 17.4142 9.25 17V10.75H3C2.58579 10.75 2.25 10.4142 2.25 10C2.25 9.58579 2.58579 9.25 3 9.25H9.25V3C9.25 2.58579 9.58579 2.25 10 2.25Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd" /></g></svg>
+                                    </button>
+                                    {labelModal.type === 'add' && labelModal.isDisplay && <LabelModal board={board} type={'add'} setLabelModal={setLabelModal} labelModal={labelModal} modalAddBtnRef={modalAddBtnRef} />}
+                                  </div>
+
+
+              </div  >
+
+
+              {!isGroupCollapsed && (
+                <>
+                  <TaskList group={group} />
+                </>
+
+              )}
 
 
               <div className="group-footer">
-                <div className='add-task '>
-                  <div className='check-label '
-                    style={{ '--before-color': group.color }}
-                  ><input type="checkbox" disabled /></div >
-                  <form onSubmit={onSubmitTask} className=''>
-                    <input className='add-task-input' type="text" placeholder='+ Add task' onBlur={onSubmitTask} onChange={handleChange} value={newTask.title} />
-                  </form>
+                {!isGroupCollapsed && (
+                  <div className='add-task '>
+                    <div className='check-label '
+                      style={{ '--before-color': group.color }}
+                    ><input type="checkbox" disabled /></div >
 
-                </div>
-                <div className="stats-labels">
-                  <div className="empty"></div>
-                  <div className="empty2"></div>
-                  <div className="content">
+                    <form onSubmit={onSubmitTask} className=''>
+                      <input className='add-task-input' type="text" placeholder='+ Add task' onBlur={onSubmitTask} onChange={handleChange} value={newTask.title} />
+                    </form>
+
+                  </div>
+                )}
+                <div className={`stats-labels ${isGroupCollapsed ? 'collapsed' : ''}`}>
+                  <>
+                    <div className={`empty ${isGroupCollapsed ? 'collapsed' : ''}`}></div>
+                    <div className={`empty2 ${isGroupCollapsed ? 'collapsed' : ''}`}></div>
+                  </>
+
+                  <div className={`content ${isGroupCollapsed ? 'collapsed' : ''}`}>
 
                     {cmpsOrder.map((cmp, idx) => {
                       if (cmp === 'StatusPicker') return (
