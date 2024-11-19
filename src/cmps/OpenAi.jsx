9037a82ee8411cde63954513ad854e852dadbf4f
zoar2307 <zoar2307@gmail.com>
2024-11-19 18:04:08 +0200

    Clean board

--- src/cmps/OpenAi.jsx
+++ src/cmps/OpenAi.jsx
@@ -286,7 +286,7 @@ export function OpenAi() {
 
         } catch (err) {
             // console.log(err)
-            showErrorMsg('Ai failed create board , try again later')
+            // showErrorMsg('Ai failed create board , try again later')
 
         }
     }
